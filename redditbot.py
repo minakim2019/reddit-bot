b49c9a37775f4a2b872defa9eced5a1f2c17163c
Mina Kim <mak132@duke.edu>
2020-09-04 01:36:17 -0400

    Output top comments per top post

--- redditbot.py
+++ redditbot.py
@@ -1,42 +1,70 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import praw
 import time
+import pandas as pd
+import textwrap
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+#configuration/oauth
 reddit = praw.Reddit("config1", user_agent="redditbot1004 user agent")
-
 subreddit = reddit.subreddit("Coronavirus")
 
-#max iterations before termination
-max_iterations = 5
+topics_dict = { "title":[], \
+                "score":[], \
+                "id":[],\
+                "created": []}
 
-# check oauth is working
-#print(reddit.user.me())
-
-# for submission in subreddit.hot(limit=5):
-#     print("Title: ", submission.title)
-#     print("Text: ", submission.selftext)
-#     print("Score: ", submission.score)
-#     print("---------------------------------\n")
-
-# primary function
-#hile(True):
-# loop through all submissions
 hot_posts = subreddit.hot(limit=10)
 for post in hot_posts:
-    print(post.title)
-    # grab the comments from that submission
-    comments = post.comments
-    top_score = 0
-    top_comment = ''
+    print(color.BOLD + post.title + color.END)
+    #specifies to not load more top comments
+    post.comments.replace_more(limit=0)
+    #iterates through top comments, printing them
+    count = 0
+    print(color.UNDERLINE + "Top Comments:" + color.END)
+    for top_level_comment in post.comments:
+        comment_text = textwrap.dedent(top_level_comment.body).strip()
+        print(textwrap.fill(comment_text, width = 60))
+        print('\n')
+        count+=1
+        if count == 2:
+            break
 
-    # loop through every comment, finding the top score
-    # checking praw.helpers.flatten_tree(comments) also checks replies
-    for comment in comments:
-        comment_text = comment.body.lower()
-        comment_score = comment.score
-        # find the top scoring comment
-        if (comment_score > top_score):
-            top_comment = comment_text
-            top_score = comment.score
+    #posts = []
+    #posts.append(
+        #[post.title, post.score, post.num_comments, post.selftext, post.created])
+    #posts = pd.DataFrame(posts,
+                            #columns=['title', 'score', 'num_comments', 'body', 'created'])
+    #print(posts)
+    #collect data
+    #topics_dict["title"].append(post.title)
+    #topics_dict["score"].append(str(post.score))
+    #topics_dict["id"].append(post.id)
+    #topics_dict["created"].append(str(post.created))
+    #topics_dict["top_comment"].append(post.)
 
 
+#date to readable timestamp format
+#def get_date(created):
+    #return dt.datetime.fromtimestamp(created)
+#_timestamp = topics_data["created"].apply(get_date)
+#topics_data = topics_data.assign(timestamp = _timestamp)
+
+
+# store everything into a file
+file = open('file.txt', 'w')#.write(output)
+
+# header to the file
+file.write('Top 10 Posts Today from r/Coronavirus' + '\n')
+file.write('-------------------------------------' + '\n\n')
